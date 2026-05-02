bdf9d6e4a6b7b5bb3f2aff3d6920383c3125a3a1
Rohan Jainendra <rohanjainendra@LT-Family-Laptop.local>
2026-05-02 11:58:43 -0700

    added memory, added datetime function and namecalling

--- python/chatbot.py
+++ python/chatbot.py
@@ -1,7 +1,7 @@
 import os 
 from langchain.memory import ConversationBufferWindowMemory
-
-from langchain.chat_models import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
+from datetime import datetime, date, timedelta
 from dotenv import load_dotenv 
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder 
@@ -10,32 +10,42 @@ from langchain_core.output_parsers import StrOutputParser
 load_dotenv()
 
 llm_model = "gpt-5-nano"
+history = []
 
 # chain: prompt | llm | str
+now = datetime.now()
+day = date.today()
+name = "TEST"
 
 prompt = ChatPromptTemplate([
     ("system", (
-        "You are helpful assistant."
+        f'''
+        You are an assistant. \n 
+        Your user is in the San Francisco Bay Area. \n
+        The time is {now}, and the date is {day}. \n
+        You should address the user by {name} \n 
+        '''
     )),
-    ("human", "{input}")
+    MessagesPlaceholder(variable_name="history"),
+    ("human", "{input}"),
 ])         
 
 llm = ChatOpenAI(model = llm_model, temperature = 0.7)
 memory = ConversationBufferWindowMemory(llm=llm)
 
 chain = prompt | llm 
-history = [human, ai]
-human = []
-ai = []
+
+
 if __name__ == "__main__" :
+    print (now, day)
     user = input("\nAsk a question or insert prompt here. Prompt chat with 'quit' to end chat.\n \nUser:")
     while True: 
-        human.append(user)
+        history.append(HumanMessage(content=user))
         if "quit" in user:
             break 
-        response = chain.invoke({"input": user})
+        response = chain.invoke({"input": user, "history": history} )
         print("\nChatGPT: " + str(response.content) + "\n")
-        ai.append(response.content)
+        history.append(AIMessage(content=response.content))
         user = input("User: ")
 
 ## tokens tell:
